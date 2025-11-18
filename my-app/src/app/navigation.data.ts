385cdfa35cea5a4f1cdf642120f35373b5855a69
Diego Soler <105398888+Dxeg0o@users.noreply.github.com>
2025-11-18 16:40:04 -0300

    feat: add tax advisory service page

--- my-app/src/app/navigation.data.ts
+++ my-app/src/app/navigation.data.ts
@@ -1,6 +1,6 @@
 export const landingNavigation = [
   { label: "Asesoría Contable", href: "asesoria-contable" },
-  { label: "Asesoría Tributaria", href: "#tributaria" },
+  { label: "Asesoría Tributaria", href: "asesoria-tributaria" },
   { label: "Asesoría en Recursos Humanos", href: "#rrhh" },
   { label: "Empresas", href: "#empresas" },
   { label: "Preguntas", href: "#faq" },
